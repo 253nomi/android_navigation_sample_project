80b86ace8d974ff5ba251c259fe5861188739962
Muhammad Nooman Ijaz <nomanijaz253@gmail.com>
2026-04-27 02:47:11 +0500

    started xml screen of same

--- app/src/main/java/com/example/androidnabigationsampleproject/MainActivity.kt
+++ app/src/main/java/com/example/androidnabigationsampleproject/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.androidnabigationsampleproject.R.layout
 import com.example.androidnabigationsampleproject.ui.theme.AndroidNabigationSampleProjectTheme
 import com.example.androidnabigationsampleproject.screens.FirstScreen
 import com.example.androidnabigationsampleproject.screens.SecondScreen
@@ -19,6 +20,7 @@ import com.example.androidnabigationsampleproject.screens.SecondScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.first_screen)
         enableEdgeToEdge()
         setContent {
             AndroidNabigationSampleProjectTheme {
